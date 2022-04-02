725e6ed38a7b972a37c48a0befc461974d1ec6fb
zsaden4 <z_saden@kbtu.kz>
2022-04-02 15:57:26 +0600

    "modified"

--- w7/1.py
+++ w7/1.py
@@ -1,21 +1,27 @@
 import pygame ,math
 import datetime
 pygame.init()
-
+#Setting the display,fps
 display=pygame.display.set_mode((800,800))
 clock=pygame.time.Clock()
 FPS=50
+
+#The backqround
 bq=pygame.image.load("mickey.jpg")
 bq=pygame.transform.scale(bq,(800,800)) 
 
 #min_arrow=pygame.image.load("left.png")
 #min_arrow=pygame.transform.scale(min_arrow,(300,300)) 
+
+#the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-i=0
+
+
+#The position function
 def convert_degrees_to_pygame(R,theta):
     y=math.cos(math.pi*theta/180)*R
     x=math.sin(math.pi*theta/180)*R
@@ -27,11 +33,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-   
+    #Setting the bq
     display.blit(bq,(0,0))
 
 
-    
+    #Getting the time
     current_time=datetime.datetime.now()
     seconds=current_time.second
     minutes=current_time.minute
