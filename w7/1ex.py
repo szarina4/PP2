fb1dfef8125e3a326231448e6b00e2824811b4b6
zsaden4 <z_saden@kbtu.kz>
2022-04-02 15:55:31 +0600

    "again"

--- w7/1ex.py
+++ w7/1ex.py
@@ -5,21 +5,21 @@ pygame.init()
 display=pygame.display.set_mode((800,800))
 clock=pygame.time.Clock()
 FPS=50
-bq=pygame.image.load("mickey.jpg")
-bq=pygame.transform.scale(bq,(800,800)) 
 
-#min_arrow=pygame.image.load("left.png")
-#min_arrow=pygame.transform.scale(min_arrow,(300,300)) 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-i=0
+
+def print_text(text,position):
+    font=pygame.font.SysFont("Castellar",40,True,False)
+    surface=font.render(text,True,BLACK)
+    display.blit(surface,position)
 def convert_degrees_to_pygame(R,theta):
     y=math.cos(math.pi*theta/180)*R
     x=math.sin(math.pi*theta/180)*R
-    return x+400+35,-(y-400)+70
+    return x+400-15,-(y-400)-15
 
 running=True
 while running:
@@ -28,24 +28,26 @@ while running:
             running = False
 
    
-    display.blit(bq,(0,0))
+    display.fill(WHITE)
+    pygame.draw.circle(display,BLACK,(400,400),400,4)
 
 
-    
+    for number in range(1,13):
+        print_text(str(number),convert_degrees_to_pygame(350,number*30))
+
     current_time=datetime.datetime.now()
     seconds=current_time.second
     minutes=current_time.minute
     
 
     #Minute
-    R=300
+    R=400
     theta=(minutes+seconds/60)*(360/60)
-    #display.blit(min_arrow,convert_degrees_to_pygame(R,theta))
     pygame.draw.line(display,BLACK,(400,400),convert_degrees_to_pygame(R,theta),6)
 
     #Second
     
-    R=350
+    R=400
     theta=seconds*(360/60)
     pygame.draw.line(display,BLUE,(400,400),convert_degrees_to_pygame(R,theta),6)
 
